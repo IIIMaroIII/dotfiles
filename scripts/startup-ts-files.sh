719e40961bba4c3cf67b9d02ce0a5a9de62f6d14
rod <matveev.r.v@icloud.com>
2025-06-29 18:22:40 +0300

    created a script typesFromTs.sh which is exploring current pwd for existing .ts, grab their name, checking if src firectory exists, with src/types, if not itll create that, and grab names up to .ts, and create .types.ts and move to the working/directory/src/types/<newNames>

--- scripts/startup-ts-files.sh
+++ scripts/startup-ts-files.sh
@@ -4,13 +4,14 @@ tsconfig=~/.dotfiles/typescript/tsconfig.json
 prettier=~/.dotfiles/.prettierrc
 gitignore=~/.dotfiles/.gitignore
 dotvscode=~/.dotfiles/.vscode
+viteConfig=~/.dotfiles/vite.config.js
 
 
 
 if [[ -d ~/.dotfiles ]]; then
     echo "➡️   The folder where the function is calling from $(pwd)"
-    cp -R "$tsconfig" "$prettier" "$gitignore" "$dotvscode" "$(pwd)"
-    echo "✅  The configs $tsconfig, $prettier, $gitignore $dotvscode have been successfully copied to the $(pwd) folder "  
+    cp -R "$tsconfig" "$prettier" "$gitignore" "$viteConfig" "$dotvscode" "$(pwd)"
+    echo "✅  The configs $tsconfig, $prettier, $gitignore $viteConfig $dotvscode have been successfully copied to the $(pwd) folder "  
 else
     echo "❌ ~/.dotfiles had not found! "
     exit 1
---
+++ scripts/typesFromTs.sh
@@ -0,0 +1,31 @@
+#!/usr/local/bin/bash
+
+
+ifSrcExists() {
+    if [[ "$1" == */src/* || "$1" == */src ]];then
+        dir=$(dirname "$1")
+        dir="${dir%%/src*}"
+        echo "$dir"
+    fi
+}
+
+curDir="$(pwd)"
+counter=0
+
+while IFS= read -r file; do
+    bsname=$(basename "$file")
+    newname=${bsname%.ts}
+    dir=$(ifSrcExists "$file")
+
+    if [[ -n "$dir" ]]; then
+        modifiedName="$newname.types.ts"
+        mkdir -p "$dir/src/types"
+        (( counter++ ))
+        touch "$dir/src/types/$modifiedName"
+        echo "➡️ ➡️ ➡️  File has been created: $dir/src/types/$modifiedName"
+
+        
+    fi
+done< <(find "$curDir" -type f -iname "*.ts" ! -iname "*.d.ts" ! -iname "main.ts")
+
+echo "✅ Total files were converted from .ts to .types.ts: $counter"
