481d953796ab55b8bb4d812a886847fb3fbe1d19
Rodion Matvieiev <matveev.r.v@icloud.com>
2026-02-20 00:49:27 +0200

    nothing

--- vite.config.js
+++ vite.config.js
@@ -1,36 +1,37 @@
-import { defineConfig } from 'vite';
-import { glob } from 'glob';
+import { defineConfig } from "vite";
+import { glob } from "glob";
 
 export default defineConfig(({ command }) => {
-	return {
-		define: {},
-		root: 'src',
-		build: {
-			sourcemap: true,
-			rollupOptions: {
-				input: glob.sync('./src/*.html'),
-				output: {
-					manualChunks(id) {
-						if (id.includes('node_modules')) {
-							return 'vendor';
-						}
-					},
-					entryFileNames: chunkInfo => {
-						if (chunkInfo.name === 'commonHelpers') {
-							return 'commonHelpers.js';
-						}
-						return '[name].js';
-					},
-					assetFileNames: assetInfo => {
-						if (assetInfo.name && assetInfo.name.endsWith('.html')) {
-							return '[name].[ext]';
-						}
-						return 'assets/[name]-[hash][extname]';
-					},
-				},
-			},
-			outDir: '../dist',
-			emptyOutDir: true,
-		},
-	};
+  return {
+    define: {},
+    root: "src",
+    build: {
+      sourcemap: true,
+      rollupOptions: {
+        input: glob.sync("./src/*.html"),
+        output: {
+          manualChunks(id) {
+            if (id.includes("node_modules")) {
+              return "vendor";
+            }
+          },
+
+          entryFileNames: (chunkInfo) => {
+            if (chunkInfo.name === "commonHelpers") {
+              return "commonHelpers.js";
+            }
+            return "[name].js";
+          },
+          assetFileNames: (assetInfo) => {
+            if (assetInfo.name && assetInfo.name.endsWith(".html")) {
+              return "[name].[ext]";
+            }
+            return "assets/[name]-[hash][extname]";
+          },
+        },
+      },
+      outDir: "../dist",
+      emptyOutDir: true,
+    },
+  };
 });
