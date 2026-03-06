c623ec7fe26516873d4d32d7b1e5a72d5d949187
Rodion Matvieiev <matveev.r.v@icloud.com>
2026-03-06 15:15:14 +0200

    r-ord

--- wsl/scripts/r-ord.sh
+++ wsl/scripts/r-ord.sh
@@ -3,15 +3,46 @@
 
 dir="$PR/$1"
 EXCLUDE_FOLDERS="(bzs|log[s])"
+order="$1"
 
-find_order=$(find "$PR" -type d -iname "$1" -regextype egrep ! -regex ".*/$EXCLUDE_FOLDERS/.*")
+[[ -z "$order" ]] && {
+	read -rp "	You didn't pass an argument. Do you wanna pass?" ans
+	if [[ "${ans,,}" == "y" ]]; then
+	read -rp "	Type the order name: " order
+	dir="$PR/$order"
+else
+	echo "	Exiting..."
+	exit 0	
+	fi
+}
+
+#echo "Order is: $order"
+#echo "Dir: $dir"
+
+
+find_order=$(find "$PR" -type d -iname "$order" -regextype egrep ! -regex ".*/$EXCLUDE_FOLDERS/.*")
 
 if [[ -z "$find_order" ]]; then 
-	echo "	Order $1 isn't found creating the structure..."
-	mkdir -p "$dir"/{src/{skp,xlsx},assets/{docs,pics,appl},send,pdfs}
-	tree -a "$dir"
+	echo "	Order $order isn't found."
+
+	read -rp "	Wanna create?	" str_ans
+	if [[ "${str_ans,,}" == "y" ]]; then
+		mkdir -p "$dir"/{src/{skp,xlsx},assets/{docs,pics,appl},send,pdfs}
+		#tree -a "$dir"
+	fi
 else
-	printf '%s\n' "Order $1 exists."
-	rm -irv "$dir"
+	printf '%s\n' "Order $order exists."
+	#rm -irv "$dir"
 	printf '%s\n' "Exiting..."
 fi
+
+copy_temp=$()
+
+read -rp "	Wanna open the $dir in explorer?	" open_dir
+
+if [[ "${open_dir,,}" == "y" ]]; then
+		explorer.exe "$(wslpath -w "$dir")"
+fi
+
+echo "	Changing the pwd..."
+cd "$dir"
