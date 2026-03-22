522f88a13f14d80d60d6677b42c19cc7744b11d5
rod <matveev.r.v@icloud.com>
2026-03-23 01:48:25 +0200

    updated bash/.bash-aliases

--- linux/arch/.config/nvim/lua/core/lazy.lua
+++ linux/arch/.config/nvim/lua/core/lazy.lua
@@ -4,18 +4,21 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git", "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
+	vim.fn.system({
+		"git", "clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
 end
 
 vim.opt.rtp:prepend(lazypath)
 
 -- Load all plugin specs from lua/plugins/*.lua
 require("lazy").setup("plugins", {
-  change_detection = { notify = false },
+	change_detection = { notify = false },
+	rocks = {
+		enabled = false
+	}
 })
