ceadfafedf5085497fb444d1420eae9435d544eb
Rodion Matvieiev <matveev.r.v@icloud.com>
2026-03-25 16:23:26 +0200

    quote mega login

--- wsl/scripts/s-mega.sh
+++ wsl/scripts/s-mega.sh
@@ -2,33 +2,36 @@
 LA_TEMPL="/mnt/c/Users/INTEL I9/AppData/Roaming/SketchUp/SketchUp 2019/LayOut/"
 SK_TEMPL="/mnt/c/Users/INTEL I9/AppData/Roaming/SketchUp/SketchUp 2019/SketchUp/"
 
-[[ $(uname -a | grep -i wsl) ]] && {
-	echo "Running on WSL in Windows"
-	cp -ru "$SK_TEMPL" "/mnt/d/projects/templates/sk_templ/"
-	cp -ru "$LA_TEMPL" "/mnt/d/projects/templates/la_templ/"
+[[ "$(uname -a | grep -i wsl)" ]] && {
+  echo "Running on WSL in Windows"
+  cp -ru "$SK_TEMPL" "/mnt/d/projects/templates/sk_templ/"
+  cp -ru "$LA_TEMPL" "/mnt/d/projects/templates/la_templ/"
 }
 echo "	Entering the mega.nz"
 
 echo "	Login into the mega.nz"
 
 if ! mega-cmd.mega-whoami &>/dev/null; then
-	mega-cmd.mega-login "$MEGA_LOGIN" "$MEGA_PASSWD"
+  mega-cmd.mega-login "$MEGA_LOGIN" "$MEGA_PASSWD"
 fi
+
+echo "  Mega login: $MEGA_LOGIN"
+echo "  Mega passwd: $MEGA_PASSWD"
 
 echo " Sync local folder to mega.nz"
 
-if [[ ! $(mega-cmd.mega-sync | grep "/mnt/d/projects") ]]; then
-	echo "	Starting sync..."
-	mega-cmd.mega-sync "/mnt/d/projects/" "/lavada/"
+if [[ ! "$(mega-cmd.mega-sync | grep "/mnt/d/projects")" ]]; then
+  echo "	Starting sync..."
+  mega-cmd.mega-sync "/mnt/d/projects/" "/lavada/"
 else
-	echo " Trying to get id of current sync"
-	id=$(mega-cmd.mega-sync |grep "/mnt/d/projects" | awk '{print $1}')
-	echo "	id: $id"
-	echo " Removing current sync by id: $id"
-	mega-cmd.mega-sync -d -- "$id"
-	echo 	" Starting new sync"
+  echo " Trying to get id of current sync"
+  id="$(mega-cmd.mega-sync | grep "/mnt/d/projects" | awk '{print $1}')"
+  echo "	id: $id"
+  echo " Removing current sync by id: $id"
+  mega-cmd.mega-sync -d -- "$id"
+  echo " Starting new sync"
 
-	mega-cmd.mega-sync "/mnt/d/projects/" "/lavada/"
+  mega-cmd.mega-sync "/mnt/d/projects/" "/lavada/"
 
-	echo "Synced at: $(date +%Y-%m-%d:%H-%M)" >> /mnt/d/projects/logs/log.md
+  echo "Synced at: $(date +%Y-%m-%d:%H-%M)" >>/mnt/d/projects/logs/log.md
 fi
